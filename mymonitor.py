64d32ebb90eeb9b29f738700d169fd98c825cb8e
Roumen B. Antonov <roumen.antonov@gmail.com>
2017-10-10 18:20:51 -0700

    Initial commit

---
+++ mymonitor.py
@@ -0,0 +1,77 @@
+import threading
+import time
+import logging
+import requests
+
+# these should be macros expanded on deployment
+COM_ERROR_CODE = 1000  # communication error
+GROK_KEY = 'bNz3SyKXTQj4fmTaMHKvKqYah4UAwkDW'
+GROK_ENDPOINT = 'https://lyra-cuw3.grokstream.com/_metrics/custom/'
+
+
+class WeatherMonitor:
+    APIXU_KEY = '2047da482cc644e482a212126170910'
+    APIXU_ENDPOINT = 'https://api.apixu.com/v1/current.json'
+
+    # defaults to be overwritten by params if necessary
+    LOCATION_Q = 'Irvine, CA'
+    TIME_INTERVAL = 2  # seconds
+
+    # these are the names of the metrics to be uploaded
+    metrics = ['temp_c', 'wind_kph', 'wind_degree', 'pressure_mb']
+
+    def __init__(self, location=LOCATION_Q, interval=TIME_INTERVAL):
+        self.next_call = time.time()
+        self.location = location
+        self.interval = interval
+        logging.basicConfig(filename='weather_metric.log', format='%(asctime)s %(message)s',
+                            level=logging.ERROR)
+
+    def start(self):
+        self.post_metric()
+
+    def post_metric(self):
+        # get the metrics
+        tstamp = time.time()
+        try:
+            data = self.get_weather()
+            # if successful - the dict is not empty
+            if len(data):
+                # post each of the metrics to Grok and log any errors
+                for key, value in data.iteritems():
+                    try:
+                        self.post_single_metric(key, tstamp, value)
+                    except requests.ConnectionError as ex:
+                        logging.error('Connectivity issue encountered when posting %s to Grok: %s.' % (key, ex.message))
+        except requests.ConnectionError as ex:
+            logging.error('Network issue encountered when accessing the weather service: %s.' % ex.message)
+
+        # After all is posted: schedule the next call
+        self.next_call = self.next_call + self.interval
+        threading.Timer(self.next_call - tstamp, self.post_metric).start()
+
+    def post_single_metric(self, metric_name, timestamp, value):
+        payload = {'timestamp': int(timestamp), 'value': value}
+        r = requests.post(GROK_ENDPOINT + metric_name, json=payload, auth=(GROK_KEY, ''), verify=False)
+        if r.status_code > 299:
+            logging.error('Posting %s metric on Grok failed with code %d. Payload = %s.' % (
+                metric_name, r.status_code, str(payload)))
+        return r.status_code
+
+    def get_weather(self):
+        """
+        :return: a number of weather related metrics in a dictionary
+
+        """
+        params = {'key': WeatherMonitor.APIXU_KEY, 'q': self.location}
+        r = requests.get(WeatherMonitor.APIXU_ENDPOINT, params=params)
+        if r.status_code > 299:
+            logging.error('Weather Service responded with code %d.' % r.status_code)
+            return {}
+        else:
+            return dict((key, r.json()['current'][key]) for key in WeatherMonitor.metrics)
+
+
+if __name__ == "__main__":
+    monitor = WeatherMonitor()
+    monitor.start()
---
+++ tests.py
@@ -0,0 +1,66 @@
+import unittest
+import time
+import random
+from mymonitor import WeatherMonitor, GROK_KEY, GROK_ENDPOINT
+import requests
+
+TEST_METRIC = 'test_metric'
+
+
+class TestWeatherMonitor(unittest.TestCase):
+    def setUp(self):
+        self.mon = WeatherMonitor()
+
+    def test_external(self):
+        data = self.mon.get_weather()
+        self.assertGreater(len(data), 0, "Weather service appears to be down or is experiencing connectivity issues.")
+        try:
+            r = requests.get(GROK_ENDPOINT, auth=(GROK_KEY, ''), verify=False)
+            self.assertLess(r.status_code, 300, 'Grok service returns status code %d.' % r.status_code)
+        except requests.ConnectionError as ex:
+            self.fail('Connectivity issues with Grok service: %s.' % ex.message)
+
+    def test_single_post(self):
+        self._delete_test_metric()
+        metrics = requests.get(GROK_ENDPOINT, auth=(GROK_KEY, ''), verify=False).json()
+        # test that te metric was deleted
+        for metric in metrics:
+            self.assertFalse(metric["name"] != TEST_METRIC)
+
+        self.mon.post_single_metric(TEST_METRIC, int(time.time()), random.random())
+
+        # get the custom metrics again after a slight delay
+        time.sleep(1)
+        metrics = requests.get(GROK_ENDPOINT, auth=(GROK_KEY, ''), verify=False).json()
+
+        # make sure that the metric was created
+        found = False
+        for metric in metrics:
+            if metric['name'] == TEST_METRIC:
+                found = True
+                break
+        self.assertTrue(found, 'Did not find the created test metric after posting.')
+
+    def test_count(self):
+        count = 10
+        self._delete_test_metric()
+        for i in range(count):
+            self.mon.post_single_metric(TEST_METRIC, int(time.time()), random.random())
+            time.sleep(1)
+        metrics = requests.get(GROK_ENDPOINT, auth=(GROK_KEY, ''), verify=False).json()
+        for metric in metrics:
+            if metric['name'] == TEST_METRIC:
+                self.assertEqual(metric['last_rowid'], count,
+                                 "Row count mismatch. Expected %d, got %d" % (count, metric['last_rowid']))
+                break
+
+    def _delete_test_metric(self):
+        try:
+            requests.delete(GROK_ENDPOINT + TEST_METRIC, auth=(GROK_KEY, ''), verify=False)
+        except Exception:
+            pass
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestWeatherMonitor)
+    unittest.TextTestRunner(verbosity=2).run(suite)
